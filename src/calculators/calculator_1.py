e01ab91d9649b6225df34d49e895b3c72b751fb0
Luan Vendt <luan.vendt@bitize.com.br>
2024-05-06 13:07:08 -0300

    Finalizando primeira calculadora

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -1,4 +1,4 @@
-from flask import requests as FlaskRequest
+from flask import request as FlaskRequest
 from typing import Dict
 
 
@@ -7,17 +7,41 @@ class Calculator1:
         body = request.json
         input_data = self.__validate_body(body)
         splited_number = input_data / 3
-        first_process_result = self.__first_process(splited_number)
 
-    def __validate_body(self, body: Dict) -> float:
+        first_process_result = self.__first_process(splited_number)
+        second_process_result = self.__second_process(splited_number)
+        calc_result = first_process_result + second_process_result + splited_number
+
+        response = self.__format_response(calc_result)
+        return response
+
+    @staticmethod
+    def __validate_body(body: Dict) -> float:
         if "number" not in body:
             raise Exception("body mal formatado!")
 
         input_data = body["number"]
         return input_data
 
-    def __first_process(self, first_number: float) -> float:
+    @staticmethod
+    def __first_process(first_number: float) -> float:
         first_part = (first_number / 4) + 7
         second_part = (first_part ** 2) * 0.257
 
         return second_part
+
+    @staticmethod
+    def __second_process(second_number: float) -> float:
+        first_part = (second_number ** 2.121)
+        second_part = (first_part / 5) + 1
+
+        return second_part
+
+    @staticmethod
+    def __format_response(calc_result: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 1,
+                "result": round(calc_result, 2)
+            }
+        }
